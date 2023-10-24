7ee76704989d822173646617f696b3cc587e903d
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-24 20:49:14 +0300

    recipe-med-info components css repositioning

--- angular/src/app/shared/components/recipe-info-med/recipe-info-med.component.ts
+++ angular/src/app/shared/components/recipe-info-med/recipe-info-med.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-info-med',
   templateUrl: './recipe-info-med.component.html',
   styleUrls: ['./recipe-info-med.component.css']
 })
-export class RecipeInfoMedComponent {
-
+export class RecipeInfoMedComponent implements AfterViewInit{
+  constructor(
+  private  Renderer2:Renderer2,
+  private  ElementRef:ElementRef,
+  ) {
+  }
+  ngAfterViewInit() {
+    this.Renderer2.setStyle(this.ElementRef.nativeElement,"display","flex")
+    this.Renderer2.setStyle(this.ElementRef.nativeElement,"justify-content","center")
+  }
 }
