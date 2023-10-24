af33c8798ff63bf97d8b37b36c866281c87c2558
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-24 20:57:24 +0300

    all recipe components are together

--- angular/src/app/shared/components/recipe-add-med-form/recipe-add-med-form.component.ts
+++ angular/src/app/shared/components/recipe-add-med-form/recipe-add-med-form.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-add-med-form',
   templateUrl: './recipe-add-med-form.component.html',
   styleUrls: ['./recipe-add-med-form.component.css']
 })
-export class RecipeAddMedFormComponent {
-
+export class RecipeAddMedFormComponent implements AfterViewInit{
+  constructor(
+    private  Renderer2:Renderer2,
+    private  ElementRef:ElementRef,
+  ) {
+  }
+  ngAfterViewInit() {
+    this.setStyles()
+  }
+  setStyles(){
+    this.Renderer2.setStyle(this.ElementRef.nativeElement,"display","flex")
+    this.Renderer2.setStyle(this.ElementRef.nativeElement,"justify-content","center")
+  }
 }
