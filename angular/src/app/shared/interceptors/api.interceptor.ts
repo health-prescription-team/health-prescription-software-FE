83e62c8da1d6d4d53444d51acfb1853bb44b88c5
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-24 19:24:27 +0300

    api interceptor

--- angular/src/app/shared/interceptors/api.interceptor.ts
+++ angular/src/app/shared/interceptors/api.interceptor.ts
@@ -6,6 +6,7 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import * as constants from "../constants";
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
@@ -13,6 +14,11 @@ export class ApiInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    const updatedRequest = request.clone({
+      url: request.url.replace("api", constants.API_URL)
+    });
+
+    // Pass the modified request to the next handler
+    return next.handle(updatedRequest);
   }
 }
