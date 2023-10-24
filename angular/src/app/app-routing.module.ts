249948e74959a9599e971dc61f8fb7cf9f76d95b
viktorShandrov <114256191+viktorShandrov@users.noreply.github.com>
2023-10-24 19:03:27 +0300

    home view

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    redirectTo:"/home"
+  },
+  {
+    path:"home",
+    pathMatch:"full",
+    component:HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
