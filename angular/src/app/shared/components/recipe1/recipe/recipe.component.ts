65fb65223fea241eab6def9fc92619492bd1568a
Veneta Ivanova <i.veneta.v@gmail.com>
2023-10-25 22:05:20 +0300

    recipe form

--- angular/src/app/shared/components/recipe1/recipe/recipe.component.ts
+++ angular/src/app/shared/components/recipe1/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe.component.css']
 })
 export class RecipeComponent {
-
+recipeInfo(form:NgForm){
+  const {patient, patientАge, doctor, diagnosis, selectMedInput, morning, midday, evening, additionalInfo}= form.value;
+  console.log(form.value);
+}
 }
