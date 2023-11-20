bf8f219eef2d64db4cbe79369f1e275592d65662
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-11-20 22:57:57 +0200

    get and send info from recipe

--- angular/src/app/shared/components/recipe1/recipe/recipe.component.ts
+++ angular/src/app/shared/components/recipe1/recipe/recipe.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import {Component, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { CacheService } from 'src/app/shared/services/cache.service';
@@ -9,6 +10,7 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./recipe.component.css'],
 })
 export class RecipeComponent implements OnInit,OnChanges {
+  
   recipeId: string = "";
   inputselectMedInput: string = '';
 
@@ -19,6 +21,7 @@ export class RecipeComponent implements OnInit,OnChanges {
   constructor(
     private service: UserService,
     public CacheService: CacheService,
+    private http:HttpClient
   ) {}
   ngOnInit(): void {
     const token = localStorage.getItem('token');
@@ -28,19 +31,53 @@ export class RecipeComponent implements OnInit,OnChanges {
     console.log("change")
   }
 
+  ageFormula(id:string){
+    //  99 01 02 3428 today:20.11.2023
+  
+
+    const array = [];
+
+       for (let i=0; i < id.length; i+=2) {
+        array.push(`${id[i]}${id[i+1]}`);
+        
+      } 
+      let[year,month,day] = array.slice(0,3);
+      const todayMonth = new Date().getMonth();
+      const todayYear = new Date().getFullYear();  
+      
+      if(Number(year) >= 23 && Number(year) <= 99){
+         year = `19${year}`
+      } else if(Number(year) >= 0 && Number(year) < Number(todayYear.toString().slice(2))) {
+        year = `20${year}`
+      }
+      return Number(todayYear) - (Number(year))
+      
+
+  }
+  years:any = ''
 
   recipeInfo(form: NgForm) {
     //POST
     const { patient, patientАge, diagnosis } = form.value;
     const allFields = { ...form.value, ...this.CacheService.nestedFormValues, "startDate":this.formattedDate };
-    console.log(allFields)
     // console.log(patient, patientАge, diagnosis);
+    
     if (!this.recipeId) {
       // response => ID
       const formData = new FormData()
       formData.append("diagnosis",allFields.diagnosis)
       formData.append("doctor",allFields.doctor)
+      // formData.append("createdAt",'20.11.2023')
       formData.append("patient",allFields.patient)
+      formData.append("endDate",allFields.endDate);
+      // formData.append("id","395ddc52-cbf9-4406-ae94-9b3eb7f5c255");
+      // to be populate in the form!
+       this.years = this.ageFormula(allFields.patient);
+      console.log(this.years);
+      
+      //  this.http.post('API/Prescription',formData).subscribe((res => console.log(res)
+      //  ))
+      
       //this.rescipeID = response.ID;
     } else {
       //put new medicine
