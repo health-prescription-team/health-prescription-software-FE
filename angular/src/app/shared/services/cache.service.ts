794e1143786a1b6e386c79227aff192da1c9b885
Veneta Ivanova <i.veneta.v@gmail.com>
2023-12-16 14:16:08 +0200

    medicine name

--- angular/src/app/shared/services/cache.service.ts
+++ angular/src/app/shared/services/cache.service.ts
@@ -1,31 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CacheService {
-  constructor(
-  ) {}
+  constructor( private router:Router) {}
   displayAddMedicine: string = 'none';
 
   showAddMedicineComponent() {
     this.displayAddMedicine = 'flex';
   }
 
-  hideAddMedicineComponent(currentmedicine:any) {
+  hideAddMedicineComponent(name:any) {
 
     this.displayAddMedicine = 'none';
-    console.log(this.nestedFormValues)
     for (const entries of Object.entries(this.nestedFormValues)) {
       // @ts-ignore
       this.nestedFormValues[entries[0]] = ""
     }
 
   }
-  addMedicine(currentmedicine:any){
+  addMedicine(currentmedicine:any, name:any){
     this.allMedicinesAdded.push({...currentmedicine})
-
-    this.hideAddMedicineComponent(currentmedicine)
+    this.hideAddMedicineComponent(name)
   }
 
 
