17ded19d0cd44b86b9a78933bdf3842bfe51c6b5
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-04 20:56:39 +0200

    add pharmacy name

--- angular/src/app/pharmacy/add-medicine/add-medicine.component.ts
+++ angular/src/app/pharmacy/add-medicine/add-medicine.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { PharmacyService } from '../services/pharmacy.service';
 import { CacheService } from 'src/app/shared/services/cache.service';
+import { UserService } from 'src/app/shared/services/user.service';
 @Component({
   selector: 'app-add-medicine',
   templateUrl: './add-medicine.component.html',
   styleUrls: ['./add-medicine.component.css']
 })
-export class AddMedicineComponent {
-constructor(private pharmacyService:PharmacyService) {}
+export class AddMedicineComponent implements OnInit {
+constructor(private pharmacyService:PharmacyService, private userService:UserService) {}
+  ngOnInit (): void {
+
+  }
 
   getFormData(formValue:any){
     const formData = new FormData()
@@ -41,13 +45,15 @@ constructor(private pharmacyService:PharmacyService) {}
   }
 
 addMedicine(form:NgForm){
-  //add company name
+  const token =localStorage.getItem('token')
+  const data = this.userService.jwtdecrypt(token!);
+
   if(form.invalid) return
   const payload = this.getFormData(form.value)
   payload.forEach(el=>console.log(el))
   payload.delete("MedicineImage")
   payload.append("MedicineImage",this.imageBinary)
-  payload.append("MedicineCompany",'name')
+  payload.append("MedicineCompany",data.unique_name)
 
 
   this.pharmacyService.addMeidicine(payload).subscribe({
