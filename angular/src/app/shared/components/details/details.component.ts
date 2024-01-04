fe90d92ade889b0efb5b64f45e26d7bdadd8b487
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-04 21:03:16 +0200

    clear blob things (not working)

--- angular/src/app/shared/components/details/details.component.ts
+++ angular/src/app/shared/components/details/details.component.ts
@@ -26,24 +26,13 @@ export class DetailsComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustResourceUrl(dataUrl);
   }
 
-  // createImageSourceFromBlob(blob: Blob) {
-  //   const reader = new FileReader();
-
-  //   reader.onload = (e) => {
-  //     // Set the data URL to the imageSource property
-  //     this.imageSrc = e.target!.result as string;
-  //   };
-
-  //   reader.readAsDataURL(blob);
-  // }
-
   ngOnInit() {
     this.ActivatedRoute.params.subscribe((params) => {
       const productId = params['id'];
       this.DetailsService.getMedicine(productId).subscribe(
         (res: any) => {
           this.currentMedicine = res;
-          console.log(this.currentMedicine);
+          console.log('this.currentMedicine', this.currentMedicine);
           this.imagebytes = res.medicineImageBytes;
 
           function binaryToPng(binary: any) {
@@ -52,76 +41,15 @@ export class DetailsComponent implements OnInit {
           }
 
           const result = binaryToPng(this.imagebytes)
-          console.log(result);
           this.imageSrc = result
 
 
           const binaryToPngResult = binaryToPng(this.imagebytes);
           this.imageSrc = binaryToPngResult
-          // console.log(binaryToPngResult);
 
 
           const reader = new FileReader();
 
-          // reader.onload = () => {
-          //   url = reader.result as string;
-          //   // console.log(url);
-
-          // };
-          // reader.readAsDataURL(blob);
-
-          // Decode the Base64 data to a Uint8Array
-          // const rawData = atob(res.medicineImageBytes);
-          // const dataAsArray = new Uint8Array(rawData.length);
-          // for (let i = 0; i < rawData.length; i++) {
-          //   dataAsArray[i] = rawData.charCodeAt(i);
-          // }
-
-          // // Decompress the GZIP data
-          //           const inflate = new Zlib.Gunzip(dataAsArray);
-          //           const decompressedData = inflate.decompress();
-
-          // // Now you have the decompressed data
-          //           const decompressedString = String.fromCharCode.apply(null, decompressedData);
-
-          // const reader = new FileReader()
-          //
-          //   const uint8Array = new Uint8Array(res.medicineImageBytes);
-          //
-          //   const textDecoder = new TextDecoder('utf-8');
-          //
-          //   const imageString = textDecoder.decode(uint8Array);
-          //
-          //   const base64Image = btoa(imageString);
-          //
-          //   console.log(base64Image);
-          // reader.onload = (e:any)=>{
-          //
-          //
-          // }
-          //   const blob = new Blob([res.medicineImageBytes], { type: 'image/png' });
-          //
-          // reader.readAsDataURL(blob)
-          // setTimeout(()=>{
-          //   console.log(this.imageSrc)
-          //   // this.imageSrc =`
-          //   // `
-          //
-          // },1000)
-
-          // this.currentMedicine.medicineImageBytes
-
-          // setTimeout(() => {
-          //   const reader = new FileReader();
-          //   const blob = new Blob([res.medicineImageBytes], { type: 'image/png' });
-          //
-          //   reader.onload = (event: any) => {
-          //     this.imageSrc = this.sanitizer.bypassSecurityTrustUrl(event.target.result);
-          //     console.log(event.target.result);
-          //   };
-          //
-          //   reader.readAsDataURL(blob); // Read the blob data, not this.imageSrc
-          // }, 0);
         },
         (error)=>{
           console.log(error)
