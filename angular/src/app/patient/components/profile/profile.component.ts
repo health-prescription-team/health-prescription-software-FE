0be5863b07fd9c9ec07caf8480df805d4c367ebc
Veneta Ivanova <i.veneta.v@gmail.com>
2024-01-08 21:49:42 +0200

    profile component

--- angular/src/app/patient/components/profile/profile.component.ts
+++ angular/src/app/patient/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { LoaderService } from 'src/app/shared/services/loader.service';
 import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
@@ -8,9 +10,14 @@ import { UserService } from 'src/app/shared/services/user.service';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  constructor(private userService: UserService,private route:ActivatedRoute) {}
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    public loaderService: LoaderService,
+    private toastr: ToastrService
+  ) {}
   name: string = '';
-  id: any;
+  id: string | null | undefined;
   egn!: string;
   prescriptions: any;
   isFulfilled: boolean = false;
@@ -18,13 +25,16 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
     const token = localStorage.getItem('token');
     const data = this.userService.jwtdecrypt(token!);
+    this.name = data.unique_name;
+    this.id = localStorage.getItem('ID');
 
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
+      // Retrieve the 'id' parameter from the URL
       this.egn = params['id'];
+      console.log('ID from URL:', this.egn);
     });
 
-
-    this.getProfile();
+    // this.getProfile();
   }
 
   getProfile() {
@@ -32,7 +42,6 @@ export class ProfileComponent implements OnInit {
       (res) => {
         console.log('res', res);
         this.prescriptions = res;
-       this.name= this.prescriptions[0].patientNames;
       },
       (err) => {
         console.log(err);
@@ -44,12 +53,12 @@ export class ProfileComponent implements OnInit {
     if (!this.prescriptions) {
       return [];
     }
-    return this.prescriptions.filter((p:any) => p.isFulfilled === this.isFulfilled);
+    return this.prescriptions.filter(
+      (p: any) => p.isFulfilled === this.isFulfilled
+    );
   }
 
   setFulfilled(value: boolean): void {
     this.isFulfilled = value;
   }
-
-
 }
--- angular/src/app/patient/patient.module.ts
+++ angular/src/app/patient/patient.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { PatientComponent } from './patient/patient.component';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './components/profile/profile.component';
+import { SharedModule } from "../shared/shared.module";
 
 
 
 
 @NgModule({
-  declarations: [
-    PatientComponent,
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    RouterModule
-  ]
+    declarations: [
+        PatientComponent,
+        ProfileComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        SharedModule
+    ]
 })
 export class PatientModule { }
