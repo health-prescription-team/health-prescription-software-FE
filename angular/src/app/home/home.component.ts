45e40089ac201e2d912e4bf65c275fa8e0539bea
Veneta Ivanova <i.veneta.v@gmail.com>
2024-02-01 20:20:22 +0200

    home logout

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CacheService } from '../shared/services/cache.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private cacheService:CacheService) {
+    this.cacheService.logout()
+
+  }
+  ngOnInit (): void {
+    // this.cacheService.logout()
+  }
 
 }
